c3e725aba0ec887e6c1c4a980185ff4742e78bd3
Remzi <ict@remzi.info>
2019-11-20 17:06:01 +0100

    Styling

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
-  <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/quiz">About</router-link>
-    </div> -->
+  <div class="container" id="app">
     <router-view/>
   </div>
 </template>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 60vh;
+}
+// Extra small devices (portrait phones, less than 576px)
+// No media query for `xs` since this is the default in Bootstrap
+
+// Small devices (landscape phones, 576px and up)
+@media (min-width: 576px) { }
+
+// Medium devices (tablets, 768px and up)
+@media (min-width: 768px) { }
+
+// Large devices (desktops, 992px and up)
+@media (min-width: 992px) { }
+
+// Extra large devices (large desktops, 1200px and up)
+@media (min-width: 1200px) {
+  #app { min-height: 90vh; }
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <img class="logo" src="../assets/money-logo.svg" alt="Money logo">
+  <div class="row">
+    <div class="col">
+      <img class="logo" src="../assets/money-logo.svg" alt="Money logo">
         <h1 class="title">
-          The Money karaoke quiz
+          World Currency Quiz
         </h1>
         <h2 class="subtitle">
           Made by Remzi Cavdar
         </h2>
-        <div id="nav">
-          <router-link to="/quiz">Start karaoke quiz</router-link>
-        </div>
-      </div>
+        <router-link to="/quiz">Start karaoke quiz</router-link>
     </div>
   </div>
 </template>
@@ -30,32 +26,25 @@
 </script>
 
 <style lang="scss">
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 .logo {
   max-width: 15vw;
   margin-bottom: 2vw;
 }
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 5vw;
   color: #35495e;
-  letter-spacing: 1px;
+  letter-spacing: 0.1vw;
+  margin: 0;
 }
 .subtitle {
   font-weight: 300;
   font-size: 3vw;
   color: #526488;
-  word-spacing: 5px;
-  margin-bottom: 2vw;
+  word-spacing: 0.5vw;
+  margin: 0.5vw 0 2vw;
 }
 </style>
+
+
