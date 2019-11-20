73cccfdb065bd0b04d65907d51e9e260a21a75fb
Remzi <ict@remzi.info>
2019-11-20 16:00:06 +0100

    Router

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/quiz">About</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
+<style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
